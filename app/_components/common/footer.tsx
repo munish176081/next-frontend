665bb9178345f956410f656d7161a7d9801a83b5
nbh174 <hi@munishsharma.in>
2025-12-01 13:07:55 +0530

    added social icons in footer and updating address in footer

--- app/_components/common/footer.tsx
+++ app/_components/common/footer.tsx
@@ -39,16 +39,21 @@ export const Footer = () => {
           </div>
         </div>
         <div className="w-3/12 max-md:w-auto flex flex-col items-end max-md:items-center gap-2">
-          <button className="bg-black text-white py-2 px-6 rounded-full text-sm font-medium hover:bg-gray-800 transition w-auto">Request a call</button>
-          <span className="text-xs font-semibold">+1 (999) 999-99-99 <br /> admin@Pups4sale.com.au</span>
+          <Link href="tel:0425408058">
+            <button className="bg-black text-white py-2 px-6 rounded-full text-sm font-medium hover:bg-gray-800 transition w-auto">Request a call</button>
+          </Link>
+          <Link   href="mailto:admin@pups4sale.com.au">
+            <button className="bg-black w-full text-white py-2 px-6 rounded-full text-sm font-medium hover:bg-gray-800 transition w-auto">Email us</button>
+          </Link>
         </div>
       </div>
       <div className="flex gap-8 max-md:flex-col max-md:items-center">
         <div className="w-3/12 flex gap-2 max-md:w-auto">
-          <a className="w-10 h-10 bg-black flex rounded-full items-center justify-center" href="https://t.me/" target="_blank" rel="noopener noreferrer"><img src="/images/vectors/sendPlane.svg" /></a>
-          <a className="w-10 h-10 bg-black flex rounded-full items-center justify-center" href="https://wa.me/" target="_blank" rel="noopener noreferrer"><img src="/images/vectors/whatsapp.svg" /></a>
+          <a className="w-10 h-10 bg-black flex rounded-full items-center justify-center" href="https://t.me/" target="_blank" rel="noopener noreferrer"><img src="/images/vectors/facebook.svg" /></a>
+          <a className="w-10 h-10 bg-black flex rounded-full items-center justify-center" href="https://t.me/" target="_blank" rel="noopener noreferrer"><img src="/images/vectors/insta.svg" /></a>
+          <a className="w-10 h-10 bg-black flex rounded-full items-center justify-center" href="https://t.me/" target="_blank" rel="noopener noreferrer"><img src="/images/vectors/pinterest.svg" /></a>
         </div>
-        <div className="w-6/12 flex gap-2 text-xs font-semibold justify-center items-center text-center">1901 Thornridge Cir. Shiloh, Hawaii 81063 - Pups4Sale</div>
+        <div className="w-6/12 flex gap-2 text-xs font-semibold justify-center items-center text-center">5/337 Settlement Rd, Thomastown, VIC, 3074</div>
         <div className="w-3/12"></div>
       </div>
     </footer>
